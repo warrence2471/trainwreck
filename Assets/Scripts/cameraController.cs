448f38a68f427a3dc1639ead911a487ee239be41
André Glüpker <git@wgmd.de>
2020-02-01 15:21:30 +0100

    Let the camera follow the player

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    public GameObject objectToFollow;
+    
+    public float speed = 3.0f;
+    public float offset = 5.0f;
+    
+    void Update () {
+        float interpolation = speed * Time.deltaTime;
+        
+        Vector3 position = this.transform.position;
+        position.z = Mathf.Lerp(this.transform.position.z, objectToFollow.transform.position.z - offset, interpolation);
+        position.x = Mathf.Lerp(this.transform.position.x, objectToFollow.transform.position.x, interpolation);
+        
+        this.transform.position = position;
+    }
+}
