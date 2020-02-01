65f8ff7136f301e2bd7fcec587e1509752295a5b
Stefan Müller <stefan.mueller@aksdb.de>
2020-02-01 13:27:14 +0100

    Added moving game train

---
+++ Assets/Scripts/GameTrain.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTrain : MonoBehaviour
+{
+    [SerializeField]
+    float MaxSpeed = 3f;
+    
+    Rigidbody myRigidbody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        myRigidbody.velocity = new Vector3(MaxSpeed, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
