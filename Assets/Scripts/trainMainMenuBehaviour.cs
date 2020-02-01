d92fdde114abf306ef02161725e091809b1f0276
Carsten Ruhr <carsten@deepl.com>
2020-02-01 09:05:15 +0100

    add main menu scene

---
+++ Assets/Scripts/trainMainMenuBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trainMainMenuBehaviour : MonoBehaviour
+{
+    private float startYposition;
+
+    private void Awake()
+    {
+        this.startYposition = this.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var newYposition = this.startYposition + (Mathf.Sin(Time.timeSinceLevelLoad * 10) + 1) / 40;
+        this.transform.position = new Vector3(this.transform.position.x, newYposition, this.transform.position.z);
+    }
+}
