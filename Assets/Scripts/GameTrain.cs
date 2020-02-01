4740065068bd72bfb54254e21cc7e7d0120ed5b9
Stefan Müller <stefan.mueller@aksdb.de>
2020-02-01 15:06:55 +0100

    Fixed train to move without Transform.velocity; Added smoke to game train

--- Assets/Scripts/GameTrain.cs
+++ Assets/Scripts/GameTrain.cs
@@ -5,20 +5,71 @@ using UnityEngine;
 public class GameTrain : MonoBehaviour
 {
     [SerializeField]
-    float MaxSpeed = 3f;
-    
-    Rigidbody myRigidbody;
+    public float maxSpeed = 1.5f;
+
+    [SerializeField]
+    public GameObject smoke;
+
+    private float age = 0.0f;
+    private float smokeAge = 0.2f;
+    private bool isDriving = false;
+    private float currentSpeed = 0;
+    private float acceleration = 0.1f;
+    private float negativeAcceleration = 0.3f;
+
+    private Rigidbody myRigidbody;
+    private Transform myTransform;
     
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody>();
-        myRigidbody.velocity = new Vector3(MaxSpeed, 0, 0);
+        myTransform = GetComponent<Transform>();
+
+        startTrain();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isDriving)
+        {
+            age += Time.deltaTime;
+            if (age > smokeAge)
+            {
+                age = 0;
+                Instantiate(smoke, new Vector3(transform.position.x + 0.4f, transform.position.y + 0.4f, transform.position.z), Quaternion.identity);
+            }
+
+            if (currentSpeed < maxSpeed)
+            {
+                currentSpeed += acceleration * Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (currentSpeed > 0)
+            {
+                currentSpeed -= negativeAcceleration * Time.deltaTime;
+            }
+        }
+
+        currentSpeed = Mathf.Clamp(currentSpeed, 0, maxSpeed);
+        myTransform.position = myTransform.position + myTransform.forward * currentSpeed / 120;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        myTransform.Rotate(0, 25f, 0);
+    }
+
+    public void startTrain()
+    {
+        this.isDriving = true;
+    }
+
+    public void stopTrain()
+    {
+        this.isDriving = false;
     }
 }
