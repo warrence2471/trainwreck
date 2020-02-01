69898b7e7c9644fbf3d71a4c801c7d99daca5522
Carsten Ruhr <carsten@deepl.com>
2020-02-01 22:20:03 +0100

    add character selector

--- Assets/Scripts/mainMenuBehaviour.cs
+++ Assets/Scripts/mainMenuBehaviour.cs
@@ -5,6 +5,40 @@ using UnityEngine.SceneManagement;
 
 public class mainMenuBehaviour : MonoBehaviour
 {
+    public GameObject maleCharacter;
+    public GameObject femaleCharacter;
+    [SerializeField]
+    private string selectedCharacter = "female";
+    private Animator maleAnimator;
+    private Animator femaleAnimator;
+
+    void Awake()
+    {
+        maleAnimator = maleCharacter.GetComponent<Animator>();
+        femaleAnimator = femaleCharacter.GetComponent<Animator>();
+
+        maleAnimator.SetBool("isVisible", true);
+        femaleAnimator.SetBool("isVisible", false);
+    }
+
+
+    public void toggleCharacter()
+    {
+        if (selectedCharacter != "male")
+        {
+            maleAnimator.SetBool("isVisible", true);
+            femaleAnimator.SetBool("isVisible", false);
+        }
+        else
+        {
+            maleAnimator.SetBool("isVisible", false);
+            femaleAnimator.SetBool("isVisible", true);
+        }
+
+        selectedCharacter = selectedCharacter == "male" ? "female" : "male";
+    }
+
+
     public void startGame()
     {
         StartCoroutine(StartGameCoroutine());
