d6978eb09d77552ebde386682d19f4e7b5f06e60
André Glüpker <git@wgmd.de>
2020-02-01 14:51:04 +0100

    Add awesome player character

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Added excitement
+        transform.position = new Vector3(
+            transform.position.x,
+            0.5f + Mathf.Sin(Time.timeSinceLevelLoad * 50) / 12,
+            transform.position.z);
+
+        // WASD + Cursor Keys
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            rb.velocity += Vector3.right * Time.deltaTime * 10;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            rb.velocity += Vector3.left * Time.deltaTime * 10;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            rb.velocity += Vector3.forward * Time.deltaTime * 10;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            rb.velocity += Vector3.back * Time.deltaTime * 10;
+        }
+
+        // "Friction"
+        rb.velocity *= 0.99f;
+
+        // Look towards mouse cursor
+        Vector3 vec = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 viewDir = new Vector3(vec.x, 0, vec.y);
+        transform.rotation = Quaternion.LookRotation(viewDir, Vector3.up);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Autsch");
+    }
+}
