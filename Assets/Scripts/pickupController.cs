5552f2ccc61981e6471a0488be449c84f95d011a
Carsten Ruhr <carsten@deepl.com>
2020-02-01 13:53:57 +0100

    general pickup script

---
+++ Assets/Scripts/pickupController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickupController : MonoBehaviour
+{
+    public string itemName;
+    public GameObject player;
+}
---
+++ Assets/Scripts/playerInventory.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerInventory : MonoBehaviour
+{
+    public List<string> items = new List<string>(2);
+    public GameObject selectedPickup;
+
+
+    private bool hasItem(string itemName)
+    {
+        return items.Contains(itemName);
+    }
+
+    public void addItem(string item, int slot)
+    {
+        items[slot] = item;
+    }
+
+    public bool useItem(string itemName)
+    {
+        if(hasItem(itemName))
+        {
+            items.Remove(itemName);
+            return true;
+        }
+
+        return false;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        { // if left button pressed...
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "pickup") {
+                    this.selectedPickup = hit.collider.gameObject;
+                }
+            }
+        }
+
+
+        if (selectedPickup)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                addItem(selectedPickup.GetComponent<pickupController>().itemName, 0);
+                Destroy(selectedPickup.gameObject);
+                selectedPickup = null;
+            }
+
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                addItem(selectedPickup.GetComponent<pickupController>().itemName, 1);
+                Destroy(selectedPickup.gameObject);
+                selectedPickup = null;
+            }
+        }
+    }
+}
