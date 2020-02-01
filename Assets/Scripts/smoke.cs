b72eb88db031d27ee5496800327ea02fbc5bac54
André Glüpker <git@wgmd.de>
2020-02-01 12:14:33 +0100

    Add simple smoke via code

---
+++ Assets/Scripts/smoke.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smoke : MonoBehaviour
+{
+    private float age = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        age += Time.deltaTime;
+        transform.position += new Vector3(0, 0, Time.deltaTime / 2);
+        if (age > 2.0f) {
+          Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/trainMainMenuBehaviour.cs
+++ Assets/Scripts/trainMainMenuBehaviour.cs
@@ -7,6 +7,9 @@ public class trainMainMenuBehaviour : MonoBehaviour
     [SerializeField]
     private float startYposition;
     [SerializeField]
+    public GameObject smoke;
+    private float age = 0.0f;
+    private float smokeAge = 0.2f;
     private float currentSpeed = 0;
     public float maxSpeed = 100;
     public float acceleration = 1;
@@ -21,6 +24,15 @@ public class trainMainMenuBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(isDriving)
+        {
+          age += Time.deltaTime;
+          if (age > smokeAge) {
+            age = 0;
+            Instantiate(smoke, new Vector3(transform.position.x+1, transform.position.y+1, transform.position.z), Quaternion.identity);
+          }
+        }
+
         if(isDriving && currentSpeed < maxSpeed)
         {
             currentSpeed += acceleration * Time.deltaTime;
