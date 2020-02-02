9e1f229a60e3ec2533c2dc96a9d4b2885e007736
Carsten Ruhr <carsten@deepl.com>
2020-02-02 13:49:48 +0100

    add instructions

--- Assets/Scripts/mainMenuBehaviour.cs
+++ Assets/Scripts/mainMenuBehaviour.cs
@@ -7,18 +7,22 @@ public class mainMenuBehaviour : MonoBehaviour
 {
     public GameObject maleCharacter;
     public GameObject femaleCharacter;
+    public GameObject instructionsPanel;
     [SerializeField]
     private string selectedCharacter = "male";
     private Animator maleAnimator;
     private Animator femaleAnimator;
+    private Animator instructionsAnimator;
 
     void Awake()
     {
         maleAnimator = maleCharacter.GetComponent<Animator>();
         femaleAnimator = femaleCharacter.GetComponent<Animator>();
+        instructionsAnimator = instructionsPanel.GetComponent<Animator>();
 
         maleAnimator.SetBool("isVisible", true);
         femaleAnimator.SetBool("isVisible", false);
+        instructionsAnimator.SetBool("instructionsVisible", false);
 
         PlayerVars.CharacterModel = selectedCharacter;
     }
@@ -45,6 +49,16 @@ public class mainMenuBehaviour : MonoBehaviour
     public void startGame()
     {
         StartCoroutine(StartGameCoroutine());
+    }
+
+    public void toggleInstructions()
+    {
+        instructionsAnimator.SetBool("instructionsVisible", !instructionsAnimator.GetBool("instructionsVisible"));
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
     }
 
     IEnumerator StartGameCoroutine()
