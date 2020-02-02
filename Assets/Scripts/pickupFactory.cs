05ef6c4fef94c0312dab3de93a293a5e3552d8d7
André Glüpker <git@wgmd.de>
2020-02-02 15:35:14 +0100

    Take ground reference for item spawns

--- Assets/Scripts/pickupFactory.cs
+++ Assets/Scripts/pickupFactory.cs
@@ -7,15 +7,15 @@ public class pickupFactory : MonoBehaviour
     public List<GameObject> pickupList = new List<GameObject>(2);
     public int numberOfObjectsToGenerate = 20;
 
-    public float maxLeft = 100;
-    public float maxTop = 100;
+    public GameObject groundToSpawn;
 
     private void Awake()
     {
+        Vector3 groundSize = groundToSpawn.transform.localScale / 2;
         for(int i = 0; i <= numberOfObjectsToGenerate; i++)
         {
-            var x = Random.Range(-maxLeft, maxLeft);
-            var z = Random.Range(-maxTop, maxTop);
+            var x = Random.Range(-groundSize.x + 1, groundSize.x - 1);
+            var z = Random.Range(-groundSize.z + 1, groundSize.z - 1);
             var itemIndex = Random.Range(0, pickupList.Count);
             var pos = new Vector3(x, 0, z);
 
