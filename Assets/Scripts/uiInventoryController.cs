d9bd21b2df26ca006713b0a1b791c3807038cf2b
André Glüpker <git@wgmd.de>
2020-02-02 13:36:09 +0100

    Make Inventory-Images more generic

--- Assets/Scripts/uiInventoryController.cs
+++ Assets/Scripts/uiInventoryController.cs
@@ -11,31 +11,34 @@ public class uiInventoryController : MonoBehaviour
     public Sprite toolSprite;
 
     public GameObject player;
-    public int ownSlot = 0;
-
     private playerInventory inventory;
 
+    private List<Image> inventorySlots = new List<Image>();
 
     void Awake()
     {
         inventory = player.GetComponent<playerInventory>();
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            var childImage = transform.GetChild(i);
+            Debug.Log(childImage + " " + childImage.name);
+            if (childImage.name.StartsWith("InventorySlot")) {
+                inventorySlots.Add(childImage.GetComponent<Image>());
+            }
+        }
     }
-
 
     // Update is called once per frame
     void Update()
     {
-        int ui_offset = 1;
-        for(int i = 0; i < transform.childCount - ui_offset; i++)
+        for(int i = 0; i < inventorySlots.Count; i++)
         {
-            var item = inventory.getItemInSlot(i);
-            var childImage = transform.GetChild(ui_offset + i);
-
-            childImage.GetComponent<Image>().sprite = GetSpriteForItem(item);
+            string item = inventory.getItemInSlot(i);
+            inventorySlots[i].sprite = GetSpriteForItem(item);
         }
     }
 
-    Sprite GetSpriteForItem(string item) {
+    public Sprite GetSpriteForItem(string item) {
         switch(item) {
             case "wood": return woodSprite;
             case "stone": return stoneSprite;
